bcd301c47a719eb0dadb94f299a19bd0c8bdbc69
Muhammad Usman <hafizusman733k@gmail.com>
2024-08-06 10:06:05 +0500

    .............................

---
+++ app/unlimited-access/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import UnlimitedAccess from '../Components/unlimitedAccessComponents/UnlimitedAccess'
+
+const page = () => {
+  return (
+    <div>
+        <UnlimitedAccess />
+    </div>
+  )
+}
+
+export default page
