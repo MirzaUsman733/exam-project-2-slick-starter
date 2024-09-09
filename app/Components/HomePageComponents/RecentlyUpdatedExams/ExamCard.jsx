67ec3244f446c6df3ce131c3db749339af4acb73
muhammad usman <hafizusman733k@gmail.com>
2024-09-09 11:52:21 +0500

    .........

--- app/Components/HomePageComponents/RecentlyUpdatedExams/ExamCard.jsx
+++ app/Components/HomePageComponents/RecentlyUpdatedExams/ExamCard.jsx
@@ -1,22 +1,24 @@
 import Link from "next/link";
 
 const ExamCard = ({ recentlyUpdated }) => {
+  const bgColors = ["bg-purple-500", "bg-green-500", "bg-orange-500"];
   return (
     <div>
       <div className="text-2xl font-bold">Recently Updated: </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 my-5">
-        {recentlyUpdated?.map((exam) => {
+        {recentlyUpdated?.map((exam, index) => {
+          const bgColor = bgColors[index % bgColors.length];
           return (
             <Link
               href={`/mock-exam/${exam?.exam_vendor_perma}/${exam?.exam_perma}`}
               key={exam?.exam_code}
               className="bg-white shadow-lg rounded-lg overflow-hidden my-2 hover:translate-y-[-5px] transition-transform duration-300"
             >
-              <div className="p-4 bg-blue-500 clip-path-polygon">
-                <h3 className="text-white text-md font-semibold">
+              <div className={`p-4 ${bgColor} clip-path-polygon`}>
+                <h3 className="text-white text-lg font-semibold">
                   {exam.exam_vendor_title} - {exam?.exam_code}
                 </h3>
-                <p className="text-white mt-2">{exam.exam_title}</p>
+                <p className="text-gray-200 mt-1">{exam.exam_title}</p>
               </div>
               <div className="flex items-center p-4">
                 <img
