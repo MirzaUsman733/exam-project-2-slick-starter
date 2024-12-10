95f87a054a3acb4319287b52dd882b8e211ce2c6
muhammad usman <hafizusman733k@gmail.com>
2024-12-10 16:57:40 +0500

    ...................

--- app/Components/Banner.jsx
+++ app/Components/Banner.jsx
@@ -1,25 +1,35 @@
+"use client";
 import Link from "next/link";
+import { useState, useEffect } from "react";
 
-const Banner = async () => {
-  const response = await fetch(
-    `${process?.env?.NEXT_PUBLIC_API_BASE_URL}/v1/banner`,
-    {
-      headers: {
-        "x-api-key": process.env.NEXT_PUBLIC_API_KEY,
-      },
-    }
-  );
-  const data = await response.json();
+const Banner = () => {
+  const [bannerData, setBannerData] = useState(null);
+
+  useEffect(() => {
+    const fetchBannerData = async () => {
+      const response = await fetch(
+        `${process?.env?.NEXT_PUBLIC_API_BASE_URL}/v1/banner`,
+        {
+          headers: {
+            "x-api-key": process.env.NEXT_PUBLIC_API_KEY,
+          },
+        }
+      );
+      const data = await response.json();
+      setBannerData(data);
+    };
+
+    fetchBannerData();
+  }, []);
 
   return (
     <div className="container mx-auto mt-5 px-6">
-      {" "}
-      {data ? (
-        <Link href={data?.banner_link} className="w-full flex justify-center">
+      {bannerData ? (
+        <Link href={bannerData.banner_link}>
           <img
-            src={data?.banner_src}
-            alt={data?.banner_website}
-            className="h-auto"
+            src={bannerData.banner_src}
+            alt={bannerData.banner_website}
+            className="w-full h-auto flex justify-center"
           />
         </Link>
       ) : (
